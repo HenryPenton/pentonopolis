0eee7b1a1a85ec900b7b99d60134d12d5ea3e4cc
Henry Penton <henry.penton@gmail.com>
2024-05-26 23:23:22 +0100

    replace code change and remove version bump step

--- shared/uuid/src/generateUUID/uuid.ts
+++ shared/uuid/src/generateUUID/uuid.ts
@@ -1,9 +1,5 @@
 import { v4 as newUUID } from "uuid";
 
 export const generateNewId = (withDashes: boolean = true): string => {
-  if (withDashes) {
-    return newUUID();
-  } else {
-    return newUUID().replaceAll("-", "");
-  }
+  return withDashes ? newUUID() : newUUID().replaceAll("-", "");
 };
