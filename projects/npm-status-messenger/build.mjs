a3768721d777f9051ec777eaa26a3a18e4c4b826
Henry Penton <henrypenton@Henrys-MacBook-Air.local>
2024-05-26 13:58:51 +0100

    exclude external dependencies

--- projects/npm-status-messenger/build.mjs
+++ projects/npm-status-messenger/build.mjs
@@ -1,4 +1,17 @@
 import * as esbuild from "esbuild";
+import fs from "fs";
+
+const fileContent = fs.readFileSync("./package.json", "utf-8");
+const { dependencies } = JSON.parse(fileContent);
+
+const dependencyMap = new Map(Object.entries(dependencies));
+
+let external = [];
+dependencyMap.forEach((dependency, key) => {
+  if (!dependency.includes("workspace")) {
+    external.push(key);
+  }
+});
 
 await esbuild.build({
   entryPoints: ["./src/startup.ts"],
@@ -7,5 +20,5 @@ await esbuild.build({
   outdir: "dist",
   platform: "node",
   target: "node22.2",
-  external: ["commander",]
+  external
 });
