aabc0bfe4a76013b58288cf05d5505f4678c846d
Henry Penton <henry.penton@gmail.com>
2024-05-27 12:45:57 +0100

    code change one

--- shared/uuid/src/generateUUID/uuid.test.ts
+++ shared/uuid/src/generateUUID/uuid.test.ts
@@ -20,9 +20,11 @@ describe("uuid", () => {
     test("the uuids are unique", () => {
       expect(generateNewId(false)).not.toEqual(generateNewId(false));
     });
+
     test("the uuids are strings", () => {
       expect(generateNewId(false)).toEqual(expect.any(String));
     });
+
     test("the uuids are the right length", () => {
       expect(generateNewId(false)).toHaveLength(32);
     });
