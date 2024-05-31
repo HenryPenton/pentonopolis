d51cb297c03403cbc23666be40d297c6fa654f9a
Henry Penton <henry.penton@gmail.com>
2024-05-31 23:56:58 +0100

    remove old trailer fetcher

--- projects/moviebot-telegram/src/fetcher/fetcher.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export type UnknownObject = { [key: string]: number | string | [] };
-type Url = string;
-
-export const fetcher = async (url: Url): Promise<UnknownObject> => {
-  return axios.get(url).then((result) => result.data);
-};
