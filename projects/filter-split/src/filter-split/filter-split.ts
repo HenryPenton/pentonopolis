b885bf2e92ce98002176636b3eab277f3adcfe2f
Henry <henry.penton@gmail.com>
2024-08-09 12:37:02 +0100

    add comment

--- projects/filter-split/src/filter-split/filter-split.ts
+++ projects/filter-split/src/filter-split/filter-split.ts
@@ -4,6 +4,7 @@ export const filterSplit = <T>(
   array: T[],
   predicate: Predicate<T>
 ): { matchingFilter: T[]; nonMatchingFilter: T[] } => {
+  //double filter not the speediest way of doing this
   const matchingFilter = array.filter(predicate);
   const nonMatchingFilter = array.filter((x) => !matchingFilter.includes(x));
 
